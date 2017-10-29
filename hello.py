0e387c3ecc9d4a748225682d9a1d6d9c36dff3f9
andryuha77 <andryuha1977@gmail.com>
2017-10-29 17:04:18 +0000

    Debug added

--- hello.py
+++ hello.py
@@ -2,8 +2,14 @@
 from flask import render_template
 from flask import Flask
 app = Flask(__name__)
+app.config['DEBUG'] = True
 
 @app.route('/')
 @app.route('/hello/<name>')
 def hello(name=None):
     return render_template('home.html', name=name)
+
+#Main method.
+if __name__ == "__main__":  
+    #Run the app.
+    app.run()
